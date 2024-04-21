897d352e03925ff596191a41b02cb883a83415ef
kszysix <kjsobkowiak@gmail.com>
2024-04-21 16:56:55 +0200

    Init

---
+++ src/main/java/com/hippobyte/shopic/ShopicApplication.java
@@ -0,0 +1,13 @@
+package com.hippobyte.shopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hippobyte/shopic/ShopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hippobyte.shopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
